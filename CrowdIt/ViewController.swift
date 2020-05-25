91aac085b7459ea937f2cd0f17ea65349a2fbc91
Ariana <arianargewurz@gmail.com>
2020-05-25 16:31:32 -0400

    Got Google Maps on Phone

--- CrowdIt/ViewController.swift
+++ CrowdIt/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["DEBUGMODE"] ?? "")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 40.807552, longitude: -73.962724, zoom: 10)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero,camera: camera)
+        view = mapView
+        
+        let currentLocation = CLLocationCoordinate2DMake(40.807552, -73.962724)
+        let marker = GMSMarker(position: currentLocation)
+        marker.title = "Skwl"
+        marker.map = mapView 
     }
 
 
