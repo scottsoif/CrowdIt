b38ff64f22ff6301571fdc1464fc067e55d25360
Scott Soifer <soifer00@gmail.com>
2020-05-25 14:19:25 -0400

    Initial Commit

---
+++ CrowdIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrowdIt
+//
+//  Created by Scott Soifer on 5/25/20.
+//  Copyright © 2020 AGS2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CrowdItTests/CrowdItTests.swift
@@ -0,0 +1,34 @@
+//
+//  CrowdItTests.swift
+//  CrowdItTests
+//
+//  Created by Scott Soifer on 5/25/20.
+//  Copyright © 2020 AGS2. All rights reserved.
+//
+
+import XCTest
+@testable import CrowdIt
+
+class CrowdItTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
